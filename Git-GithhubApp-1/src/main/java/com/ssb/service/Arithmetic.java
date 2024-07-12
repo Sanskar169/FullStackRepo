97c0a5b54e03937315d0388d8e3fd91f4ad707c1
sanskar <sanskarburange1@gmail.com>
2024-07-12 19:17:13 +0530

    First Share

---
+++ Git-GithhubApp-1/src/main/java/com/ssb/service/Arithmetic.java
@@ -0,0 +1,7 @@
+package com.ssb.service;
+
+public class Arithmetic {
+	public int sum(int a,int b) {
+		return a+b;
+	}
+}
---
+++ Git-GithhubApp-1/src/test/java/com/ssb/test/AppTest.java
@@ -0,0 +1,18 @@
+package com.ssb.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.ssb.service.Arithmetic;
+
+public class AppTest 
+{
+	@Test
+	public void testWithPositive() {
+		Arithmetic ar=new Arithmetic();
+		int expected=40;
+		int actual=ar.sum(20, 20);
+		assertEquals(expected, actual);
+	}
+}
