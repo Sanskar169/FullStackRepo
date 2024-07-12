b742824231263de304ee7d8ed65ed2c30b4163aa
sanskar <sanskarburange1@gmail.com>
2024-07-12 19:39:23 +0530

    TL Modify

--- Git-GithhubApp-1/src/main/java/com/ssb/service/Login.java
+++ Git-GithhubApp-1/src/main/java/com/ssb/service/Login.java
@@ -1,5 +1,7 @@
 package com.ssb.service;
 
 public class Login {
-
+	public boolean authenticate(String username,String password) {
+		return username=="Sanskar169" && password=="Sanskar169"?true:false;
+	}
 }
